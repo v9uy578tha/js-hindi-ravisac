7e7b01a8405209284faa895129265a08fb20e0b8
v9uy578tha <109132922+v9uy578tha@users.noreply.github.com>
2023-12-12 14:29:11 +0000

    var ,const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144557
+let accountEmail="example@gmail.com"//let and var are for declaring variables,const is for declaring constants
+var accountPassword="12345"//prefer not to use var for declaration
+accountCity ="Bhubaneswar"
+
+accountEmail="hc@gmail.com"
+accountPassword="34896"
+accountCity="Cuttack"
+// accountId=3
+// console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity])
