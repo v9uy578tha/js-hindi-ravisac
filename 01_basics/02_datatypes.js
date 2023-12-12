ea7c7d782745733cbe61803f8aa858c4e0037276
v9uy578tha <109132922+v9uy578tha@users.noreply.github.com>
2023-12-12 14:51:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+// alert("hello")//we are using node js not browser
+//code readability should high
+let name="raj"//string datatype
+let age=18 //number datatype
+let islogggedin=false //boolean datatype
+//bigint for bigger number
+//number =>2 to power 53
+//boolean => true/false
+//null =>standalone value
+//undefined =>
+// symbol =>unique
+//object
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof islogggedin);
+
+
